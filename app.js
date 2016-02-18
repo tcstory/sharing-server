9b6535d249e88e578e520071e1d70cb5a0f9c882
tiancui <tcstory@163.com>
2016-02-18 20:11:24 +0800

    完成游客的基本聊天功能

--- app.js
+++ app.js
@@ -17,12 +17,13 @@ var visitorList = [];
 var configMap = require('./config.js');
 
 app.use(require('cookie-parser')());
-app.use(require('express-session')({
+var session = require('express-session')({
         secret: 'tcstory',
         resave: true,
         saveUninitialized: false
     }
-));
+);
+app.use(session);
 app.use(express.static(path.join(__dirname, 'site', 'dist')));
 app.get('/', function (req, res) {
     if (req.session.userId) {
@@ -31,6 +32,9 @@ app.get('/', function (req, res) {
         //res.cookie('room', '10001', {
         //    httpOnly: true
         //});
+        if (process.env.NODE_ENV === 'development') {
+            console.log(visitorList);
+        }
     } else {
         req.session.visitorId = Utils.createIdForVisitor(visitorList);
         req.session.userName = req.session.visitorId;
@@ -44,9 +48,10 @@ app.get('/', function (req, res) {
 var userRouter = require('./routers/user.js');
 app.use('/serv/user', userRouter);
 
-
+var sharedsession = require("express-socket.io-session");
+io.use(sharedsession(session));
 io.on('connection', function(socket){
-    console.log('a user connect');
+    socket.join(socket.handshake.session.curRoom);
     socket.emit('user list', Utils.getCurUserList());
     socket.emit('activities', Utils.getActivities());
     socket.emit('chat messages', Utils.getMessages());
@@ -54,10 +59,10 @@ io.on('connection', function(socket){
     socket.on('disconnect', function(){
     });
     socket.on('chat message', function (msg) {
-        socket.emit('chat messages', {
-            userId: '',
-            userAvatar: '',
-            userName: '中华田园犬',
+        io.to(socket.handshake.session.curRoom).emit('chat messages', {
+            userId: socket.handshake.session.userId,
+            userAvatar: socket.handshake.session.userAvatar,
+            userName: socket.handshake.session.userName,
             content: msg
         });
     });
