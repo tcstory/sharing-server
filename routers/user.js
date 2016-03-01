82b3a18ab29be98e2dde9b83308efb43a5646624
tiancui <tcstory@163.com>
2016-03-02 01:09:18 +0800

    基本完成用户资料的修改功能

--- routers/user.js
+++ routers/user.js
@@ -11,7 +11,6 @@ var Utils = require('../utils');
 var configMap = require('../config.js');
 
 
-
 router.post('/sign-in', bodyParser.json(), function (req, res) {
     Utils.cros(res);
     res.set('Content-Type', 'application/json');
@@ -32,6 +31,7 @@ router.post('/sign-in', bodyParser.json(), function (req, res) {
                 req.session.userName = doc.userName;
                 req.session.userId = doc.userId;
                 req.session.userAvatar = doc.userAvatar;
+                req.session.userIntro = doc.userIntro;
                 global.dbInstance.collection('onlinePeople').updateOne({
                     userId: previousPeople
                 }, {
@@ -73,31 +73,31 @@ router.post('/sign-up', bodyParser.json(), function (req, res) {
         if (!err) {
             if (!result) {
                 global.dbInstance.collection('user').find().count(function (err, result) {
-                   if (!err) {
-                       var userId = configMap.userIdPrefixCharacter + (configMap.userIdStartNumber + result);
-                       global.dbInstance.collection('user').insertOne({
-                           userName: userName,
-                           password: password,
-                           userId: userId,
-                           userAvatar: configMap.defaultAvatar
-                       });
-                       var previousPeople = req.session.userId;
-                       req.session.userName = userName;
-                       req.session.userId = userId;
-                       req.session.userAvatar = configMap.defaultAvatar;
-                       global.dbInstance.collection('onlinePeople').updateOne({
-                           userId: previousPeople
-                       }, {
-                           $set: {
-                               userName: userName,
-                               userId: userId,
-                               userAvatar: configMap.defaultAvatar
-                           }
-                       });
-                       res.send({
-                           code: configMap.statusCode.ok
-                       })
-                   }
+                    if (!err) {
+                        var userId = configMap.userIdPrefixCharacter + (configMap.userIdStartNumber + result);
+                        global.dbInstance.collection('user').insertOne({
+                            userName: userName,
+                            password: password,
+                            userId: userId,
+                            userAvatar: configMap.defaultAvatar
+                        });
+                        var previousPeople = req.session.userId;
+                        req.session.userName = userName;
+                        req.session.userId = userId;
+                        req.session.userAvatar = configMap.defaultAvatar;
+                        global.dbInstance.collection('onlinePeople').updateOne({
+                            userId: previousPeople
+                        }, {
+                            $set: {
+                                userName: userName,
+                                userId: userId,
+                                userAvatar: configMap.defaultAvatar
+                            }
+                        });
+                        res.send({
+                            code: configMap.statusCode.ok
+                        })
+                    }
                 });
             } else {
                 res.send({
@@ -111,19 +111,19 @@ router.post('/sign-up', bodyParser.json(), function (req, res) {
         }
     })
 });
-router.get('/sign-out', function (req,res) {
+router.get('/sign-out', function (req, res) {
     Utils.cros(res);
     if (req.session.userId.length != 0) {
-       global.dbInstance.collection('onlinePeople').deleteOne({
-           userId: {
-               $eq: req.session.userId
-           }
-       });
-       req.session.destroy();
-       res.send({
-           code: configMap.statusCode.ok
-       })
-   }
+        global.dbInstance.collection('onlinePeople').deleteOne({
+            userId: {
+                $eq: req.session.userId
+            }
+        });
+        req.session.destroy();
+        res.send({
+            code: configMap.statusCode.ok
+        })
+    }
 });
 
 router.get('/basic-info', function (req, res) {
@@ -135,6 +135,7 @@ router.get('/basic-info', function (req, res) {
         obj.userId = req.session.userId;
         obj.userAvatar = req.session.userAvatar;
         obj.userName = req.session.userName;
+        obj.userIntro = req.session.userIntro;
         var projection = {};
         //projection[req.session.curRoom] = 1;
         projection['_id'] = 0;
@@ -142,7 +143,7 @@ router.get('/basic-info', function (req, res) {
             roomId: {
                 $eq: req.session.curRoom
             }
-        },projection).next(function (err, doc) {
+        }, projection).next(function (err, doc) {
             if (doc) {
                 obj.roomLogo = doc.roomLogo;
                 obj.roomDescription = doc.roomDescription;
@@ -153,6 +154,87 @@ router.get('/basic-info', function (req, res) {
     }
 });
 
+var fs = require('fs');
+var path = require('path');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'site/dist/assets/user/')
+    },
+    filename: function (req, file, cb) {
+
+        req.body.avatarFileName = Date.now() + '.' + file.mimetype.split('/')[1];
+        req.body.avatarSuffix = file.mimetype.split('/')[1];
+        cb(null, req.body.avatarFileName);
+    }
+});
+var upload = multer({
+    storage: storage
+});
+var storePath = process.cwd() + '/site/dist/assets/user';
+
+router.post('/modify-data', upload.single('userAvatar'), function (req, res) {
+    if (req.file) {
+        try {
+            fs.mkdirSync(path.join(storePath, req.session.userId));
+        } catch (e){
+            console.log(e);
+        }
+        fs.writeFileSync(path.join(storePath, req.session.userId, 'avatar.' + req.body.avatarSuffix), fs.readFileSync(path.join(storePath, req.body.avatarFileName)));
+        fs.unlinkSync(path.join(storePath, req.body.avatarFileName));
+        req.session.userAvatar = 'assets/user/' + req.session.userId + '/avatar.' + req.body.avatarSuffix;
+        global.dbInstance.collection('user').updateOne({userId: req.session.userId}, {
+            $set: {
+                userAvatar: req.session.userAvatar
+            }
+        });
+    }
+    global.dbInstance.collection('user').find({userName: req.body.userName}).next(function (err, doc) {
+        if (doc) {
+            if (doc.userId !== req.session.userId) {
+                res.send({
+                    code: configMap.statusCode.error,
+                    msg: {
+                        content: '用户名已经存在',
+                        title: '错误'
+                    }
+                })
+            } else {
+                global.dbInstance.collection('user').updateOne({userId: req.session.userId}, {
+                    $set: {
+                        userName: req.body.userName,
+                        userIntro: req.body.userIntro
+                    }
+                });
+                req.session.userName = req.body.userName;
+                req.session.userIntro = req.body.userIntro;
+                res.send({
+                    code: configMap.statusCode.ok,
+                    msg: {
+                        content: '修改成功',
+                        title: '报喜'
+                    }
+                })
+            }
+        } else {
+            global.dbInstance.collection('user').updateOne({userId: req.session.userId}, {
+                $set: {
+                    userName: req.body.userName,
+                    userIntro: req.body.userIntro
+                }
+            });
+            req.session.userName = req.body.userName;
+            req.session.userIntro = req.body.userIntro;
+            res.send({
+                code: configMap.statusCode.ok,
+                msg: {
+                    content: '修改成功',
+                    title: '报喜'
+                }
+            })
+        }
+    });
+});
 
 router.options('/*?', function (req, res) {
     Utils.cros(res);
